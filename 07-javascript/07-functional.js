6de98a9368508d8b9b2abb9642cb99c27a3abc64
meghanasharathd <146280196+meghanasharathd@users.noreply.github.com>
2024-03-11 10:48:25 -0700

    update

--- 07-javascript/07-functional.js
+++ 07-javascript/07-functional.js
@@ -22,20 +22,22 @@ const characters = [
 const mapNameFamily = (characters) => {
   // Return an array with the name and house of each character
   // Format: "Arya, of House Stark"
-
-  return characters;
-};
+ characters =characters.map(character => `${character.name}, of House ${character.house}`)
+  return characters;  
+}
 
 const filterFamily = (characters, house) => {
   // Return an array with only the characters from a given house
-
+   characters = characters.filter(character => character.house === house);
   return characters;
 };
 
 const reduceHouses = (characters) => {
   // Return an object with the number of characters from each house
-
-  return characters;
+  return characters.reduce((num, character) => {
+    num[character.house] = (num[character.house] || 0) + 1;
+    return num;
+  }, {});
 };
 
 console.log(mapNameFamily(characters));
--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,52 +1,87 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+// const url = 'https://anapioficeandfire.com/api/books/';
 
-const app = document.querySelector('#books');
-app.style.paddingLeft = 0;
-const loading = document.querySelector('#loading');
+// const app = document.querySelector('#books');
+// app.style.paddingLeft = 0;
+// const loading = document.querySelector('#loading');
 
-const addBookToDOM = (item) => {
-  console.log(item);
-  let element = document.createElement('div');
-  let title = document.createElement('h4');
-  let author = document.createElement('p');
-  let published = document.createElement('p');
-  let pages = document.createElement('p');
+// const addBookToDOM = (item) => {
+//   console.log(item);
+//   let element = document.createElement('div');
+//   let title = document.createElement('h4');
+//   let author = document.createElement('p');
+//   let published = document.createElement('p');
+//   let pages = document.createElement('p');
 
-  element.style.display = 'flex';
-  element.style.flexDirection = 'column';
-  element.style.alignItems = 'center';
-  element.style.marginTop = '20px';
+//   element.style.display = 'flex';
+//   element.style.flexDirection = 'column';
+//   element.style.alignItems = 'center';
+//   element.style.marginTop = '20px';
 
-  title.textContent = item.name;
-  author.textContent = `by ${item.authors[0]}`;
-  published.textContent = item.released.substr(0, 4);
-  pages.textContent = `${item.numberOfPages} pages`;
+//   title.textContent = item.name;
+//   author.textContent = `by ${item.authors[0]}`;
+//   published.textContent = item.released.substr(0, 4);
+//   pages.textContent = `${item.numberOfPages} pages`;
 
-  element.append(title);
-  element.append(author);
-  element.append(published);
-  element.append(pages);
+//   element.append(title);
+//   element.append(author);
+//   element.append(published);
+//   element.append(pages);
 
-  app.append(element);
-};
+//   app.append(element);
+// };
 
-const fetchData = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        addBookToDOM(item);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      let li = document.createElement('li');
-      li.textContent = `An error occured. Please try again.`;
-      app.append(li);
-    })
-    .finally(() => {
-      app.removeChild(loading);
+// const fetchData = (url) => {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.forEach((item) => {
+//         addBookToDOM(item);
+//       });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       let li = document.createElement('li');
+//       li.textContent = `An error occured. Please try again.`;
+//       app.append(li);
+//     })
+//     .finally(() => {
+//       app.removeChild(loading);
+//     });
+// };
+
+// fetchData(url);
+
+$(document).ready(function() {
+  // API endpoint for fetching books
+  const apiUrl = "https://anapioficeandfire.com/api/books";
+
+  // Function to fetch books from the API
+  function fetchBooks() {
+    $.ajax({
+      url: apiUrl,
+      method: "GET",
+      success: function(response) {
+        // Loop through each book in the response
+        response.forEach(function(book) {
+          // Create elements for each book
+          let bookElement = $("<div>").addClass("book");
+          let titleElement = $("<h3>").text(book.name);
+          let authorElement = $("<p>").text("Author: " + book.authors.join(", "));
+          let releasedElement = $("<p>").text("Released: " + book.released);
+
+          // Append elements to the book container
+          bookElement.append(titleElement, authorElement, releasedElement);
+
+          // Append book container to the DOM
+          $("#booksContainer").append(bookElement);
+        });
+      },
+      error: function(error) {
+        console.log("Error fetching books:", error);
+      }
     });
-};
+  }
 
-fetchData(url);
+  // Call the fetchBooks function to fetch and append books when the document is ready
+  fetchBooks();
+});
